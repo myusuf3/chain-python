90775f7d9c9609a966700bc444c4b4501be28436
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2014-07-20 19:23:01 -0400

    adding stub for tests :smile:

---
+++ tests/test_chain.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import mock
+import chain
+
+class ChainTests(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
