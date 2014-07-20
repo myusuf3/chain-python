fae75089ae63f6c1b64b331626c9cf2f12c2e002
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2014-07-19 23:38:59 -0400

    adding setup.py for the file

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+required = ['requests']
+
+setup(
+    name='chain-python',
+    version='0.0.1',
+    description='chain.com Unofficial API Wrapper.',
+    long_description=open('README.md').read(),
+    author='Mahdi Yusuf',
+    author_email='yusuf.mahdi@gmail.com',
+    url='https://github.com/myusuf3/chain-python',
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+    ),
+)
