f880e44b5bcfb5602386f5b6b898c8cea5af3bb6
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2014-07-20 17:20:55 -0400

    adding all stuff needed for transactions

--- chain/core.py
+++ chain/core.py
@@ -4,7 +4,7 @@
 
 import requests
 
-DEFAULT_CHAIN_URL = 'https://api.chain.com/'
+DEFAULT_CHAIN_URL = 'https://api.chain.com/{version}/{blockchain}'
 
 
 class Chain(object):
@@ -15,21 +15,81 @@ class Chain(object):
         self.session.auth = (api_key, '')
         self.blockchain = blockchain
         self.api_version = version
+        self.base_url = DEFAULT_CHAIN_URL.format(version=self.api_version,
+            blockchain=self.blockchain,
+        )
 
     def _get_response(self, path, data={}):
-        data = data.update({'key': self.api_key})
+        path = self.base_url + path
+        data.update({'key': self.api_key})
         response = self.session.get(path, params=data)
+        print response.request.url
         return response
 
+
     def get_address(self, address):
-        path = "/{version}/{blockchain}/addresses/{address}".format(
-            version=self.api_version,
-            blockchain=self.blockchain,
+        path = "/addresses/{address}".format(
             address=address
         )
-        path = DEFAULT_CHAIN_URL + path
         response = self._get_response(path)
         return response.json()
 
     def get_addresses(self, addresses):
-        return self.get_address(','.join(addresses))
+        if len(addresses) <= 200:
+            return self.get_address(','.join(addresses))
+        else:
+            raise ValueError('You provided more than the allowed 200 addresses')
+
+
+    def get_address_transaction(self, address, limit=50):
+        data = {'limit': limit}
+        path = "/addresses/{address}/transactions".format(
+            address=address,
+            )
+        response = self._get_response(path, data=data)
+        return response.json()
+
+    def get_address_transactions(self, addresses, limit=50):
+        if len(addresses) <= 200:
+            return self.get_address_transaction(','.join(addresses), limit=limit)
+        else:
+            raise ValueError('You provided more than the allowed 200 addresses')
+
+    def get_address_unspents(self, address):
+        path = "/addresses/{address}/unspents".format(address=address)
+        response = self._get_response(path)
+        return response.json()
+
+    def get_addresses_unspents(self, addresses):
+        if len(addresses) <= 200:
+            return self.get_address_unspents(','.join(addresses))
+        else:
+            raise ValueError('You provided more than the allowed 200 addresses')
+
+    def get_address_op_returns(self, address):
+        path =  '/addresses/{address}/op-returns'.format(address=address)
+        response = self._get_response(path)
+        return response.json()
+
+    def get
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
